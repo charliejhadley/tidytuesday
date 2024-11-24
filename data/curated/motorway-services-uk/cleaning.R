076e06525c0ff5f24a406664ac6708fd95e78610
charliejhadley <charliejhadley@gmail.com>
2024-11-24 12:30:00 +0000

    removed references to ireland

--- data/curated/motorway-services-uk/cleaning.R
+++ data/curated/motorway-services-uk/cleaning.R
@@ -295,14 +295,11 @@ data_process_ops_simple_all <- data_services_info %>%
   count(operator, name = "n_named_sites_all") 
 
 data_operators <- data_process_ops_simple_all %>% 
-  left_join(data_process_ops_simple_ireland) %>% 
   left_join(data_process_ops_single) %>% 
   left_join(data_process_ops_twins) %>% 
   left_join(data_process_ops_pair) %>% 
   mutate(across(everything(), ~replace_na(.x, 0))) %>% 
-  mutate(n_named_sites_mainland = n_named_sites_all - n_named_sites_ireland) %>% 
   select(
     operator,
-    n_named_sites_all,
-    n_named_sites_mainland,
+
     everything())
